ae0015b4b51c58b8bb573ce661825bd073f86476
LeoPedroso2323 <leonardopedroso857@gmail.com>
2024-10-02 11:25:14 -0300

    02-10

--- 25-09/src/controllers/userController.js
+++ 25-09/src/controllers/userController.js
@@ -9,9 +9,11 @@ module.exports = class userController {
         .status(400)
         .json({ error: "Todos os campos devem ser preenchidos" });
     } else if (isNaN(cpf) || cpf.length !== 11) {
-      return res.status(400).json({
-        error: "CPF inválido. Deve conter exatamente 11 dígitos numéricos",
-      });
+      return res
+        .status(400)
+        .json({
+          error: "CPF inválido. Deve conter exatamente 11 dígitos numéricos",
+        });
     } else if (!email.includes("@")) {
       return res.status(400).json({ error: "Email inválido. Deve conter @" });
     }
@@ -38,45 +40,36 @@ module.exports = class userController {
   }
 
   static async updateUser(req, res) {
-    // Desestrutura e recupera os dados enviados via corpo da requisição
+    // desestrutura e recupera os dados enviados via corpo da requisição
     const { cpf, email, password, name } = req.body;
-
-    //Validar se todos os campos foram preenchidos
     if (!cpf || !email || !password || !name) {
+      // valida se todos os campos foram preenchidos
       return res
         .status(400)
         .json({ error: "Todos os campos devem ser preenchidos" });
     }
-
-    //Procurar o indice do usuario pelo na Array 'users' pelo cpf
+    // procura indice do user no array 'users' pelo cpf
     const userIndex = users.findIndex((user) => user.cpf === cpf);
-
-    //Se o usuário não for encontrado userIndex equivale a -1
+    // se não for encontrado o 'userindex' equivale a -1
     if (userIndex === -1) {
-      return res.status(400).json({ error: "Usuario não encontrado" });
+      return res.status(400).json({ error: "Usuário não encontrado" });
     }
-
-    //Atualiza os dados do usuario no Array 'users'
-    users[userIndex] = { cpf, email, password, name };
-
+    // atualiza os dados do usuario na array 'users'
+    users[userIndex] = { cpf, email, password, email };
     return res
       .status(200)
-      .json({ message: "Usuario atualizado", users: [userIndex] });
+      .json({ message: "Usuário atualizado", user: users[userIndex] });
   }
 
   static async deleteUser(req, res) {
-    // Obtém o parametro 'id' da requisição, que é o CPF do user a ser deletado
     const userId = req.params.cpf;
-
-    //Procurar o indice do usuario pelo na Array 'users' pelo cpf
-    const userIndex = users.findIndex((user) => user.cpf === cpf);
-
-    //Se o usuário não for encontrado userIndex equivale a -1
+    const userIndex = users.findIndex((user) => user.cpf === userId);
+    // se não for encontrado o 'userindex' equivale a -1
     if (userIndex === -1) {
-      return res.status(400).json({ error: "Usuario não encontrado" });
+      return res.status(400).json({ error: "Usuário não encontrado" });
     }
-    // Removendo o usuario do Array 'users'
-    users.splice(userIndex, 1); 
-    return res.status(200).json({ message: "Usuário apagado", users});
+    // removendo usuário da array 'users'
+    users.splice(userIndex, 1) // começa no indice 'userIndex', e apaga somente '1'
+    return res.status(200).json({ message: "Usuário apagado", users });
   }
 };
