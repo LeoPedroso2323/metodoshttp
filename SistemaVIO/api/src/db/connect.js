47f9d2292f182f20ed94c06ca0d28860cb75b8f0
LeoPedroso2323 <leonardopedroso857@gmail.com>
2024-11-06 08:33:00 -0300

    06-11

--- SistemaVIO/api/src/db/connect.js
+++ SistemaVIO/api/src/db/connect.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
+  connectionLimit: 10,
   host: "localhost",
   user: "alunods",
   password: "senai@604",
-  database: "vio",
+  database: "vio_leonardo",
 });
 
 module.exports = pool;
